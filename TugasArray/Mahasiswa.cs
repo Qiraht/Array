55841448a7d9ca279dad783db5c20491450b8a44
Qiraht <48444430+Qiraht@users.noreply.github.com>
2019-06-18 12:19:53 +0700

    Add files via upload

---
+++ TugasArray/Mahasiswa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasArray
+{
+    public class Mahasiswa
+    {
+        string[] arrNim = { "1234", "1234", "1234", };
+        string[] arrNama = { "erick", "wely", "sony" };
+        int[] arrNilai = { 75, 80, 95 };
+
+        public void Show()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("{0}. {1} \t ", i + 1, arrNim[i]);
+                Console.Write("\t {0} \t {1} ", arrNama[i], arrNilai[i]);
+                Console.WriteLine();
+            }
+        }
+}
+}
---
+++ TugasArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+               Mahasiswa  mhs = new Mahasiswa
+                Console.WriteLine("No. Nim \t Nama \t Nilai");
+                Console.WriteLine("===========================================");
+                mhs.Show();
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
